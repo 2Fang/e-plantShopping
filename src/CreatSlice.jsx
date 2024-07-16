bfe220198260c54fedc7f06fc59d5e1baa4142c7
2Fang <50331150+2Fang@users.noreply.github.com>
2024-07-16 12:40:59 +0100

    Update CreatSlice.jsx

--- src/CreatSlice.jsx
+++ src/CreatSlice.jsx
@@ -1,20 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CreatSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
+        visible: false,
     },
-    removeItem: (state, action) => {
+    reducers: {
+        addItem: (state, action) => {
+            const { name, image, cost } = action.payload;
+            console.log(`Adding item ${name}`)
+            const existingItem = state.items.find(item => item.name === name);
+            if (existingItem) {
+                existingItem.quantity++;
+            } else {
+                state.items.push({ name, image, cost, quantity: 1 });
+            }
+            console.log(`Items in cart: ${state.items.length}`);
+        },
+        removeItem: (state, action) => {
+            console.log(`Removing ${JSON.stringify(action)}`);
+            state.items = state.items.filter(item => item.name !== action.payload.name);
+        },
+
+        updateQuantity: (state, action) => {
+            const { name, quantity } = action.payload;
+            const itemToUpdate = state.items.find(item => item.name === name);
+            console.log(`${JSON.stringify(itemToUpdate)}`);
+            console.log(`Payload ${quantity}`)
+            if (itemToUpdate) {
+                itemToUpdate.quantity = quantity;
+                console.log(`New object: ${JSON.stringify(itemToUpdate)}`);
+            }
+        },
     },
-    updateQuantity: (state, action) => {
-    
-    },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CreatSlice.actions;
